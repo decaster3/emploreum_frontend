7f378ad4578d0c33f75a41f9c40664859b969a63
rinat <tiran678@icloud.com>
2018-01-25 20:36:07 +0300

    sequelize added

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const logger = require('./logger');
-
 const argv = require('./argv');
 const port = require('./port');
 const setup = require('./middlewares/frontendMiddleware');
@@ -10,6 +9,74 @@ const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
 const app = express();
+const db = require('./models');
+
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize('emploreum-dev', 'postgres', 'postgres', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   port: 5432,
+//
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+//
+//   operatorsAliases: false,
+// });
+
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//   })
+//   .catch((err) => {
+//     console.error('Unable to connect to the database:', err);
+//   });
+
+// const Client = sequelize.define('client', {
+//   username: Sequelize.STRING,
+//   birthday: Sequelize.DATE,
+// });
+
+// const Usser = sequelize.define('usser', {
+//   firstName: {
+//     type: Sequelize.STRING,
+//   },
+//   lastName: {
+//     type: Sequelize.STRING,
+//   },
+// });
+
+// force: true will drop the table if it already exists
+// Usser.sync({ force: true }).then(() =>
+//   // Table created
+//   Usser.create({
+//     firstName: 'John',
+//     lastName: 'Hancock',
+//   }));
+
+// sequelize.sync()
+//   .then(() => Client.create({
+//     username: 'janedoe',
+//     birthday: new Date(1980, 6, 20),
+//   }))
+//   .then((jane) => {
+//     console.log(jane.toJSON());
+//   });
+
+// app.get('/users', (req, res) => {
+//   console.log('INSIDE GET METHOD');
+//   Client.findOne({
+//     where: { id: '2' },
+//     attributes: ['id', ['username', 'title']],
+//   }).then((project) => {
+//     if (project != null) { res.send(project); } else res.send('null');
+//     // project will be the first entry of the Projects table with the title 'aProject' || null
+//   });
+// });
 
 setup(app, {
   outputPath: resolve(process.cwd(), 'build'),
@@ -21,13 +88,10 @@ const customHost = argv.host || process.env.HOST;
 const host = customHost || null; // Let http.Server use its default IPv6/4 host
 const prettyHost = customHost || 'localhost';
 
-console.log('werfueirufheirufeiur');
-console.log(port);
-console.log('werfueirufheirufeiur');
-
 // Start your app.
 app.listen(port, host, (err) => {
   console.log(port);
+  db.sequelize.sync();
   if (err) {
     return logger.error(err.message);
   }
---
+++ server/migrations/20180125111138-create-employee.js
@@ -0,0 +1,44 @@
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Employees', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    password: {
+      type: Sequelize.STRING,
+    },
+    rating: {
+      type: Sequelize.FLOAT,
+    },
+    photo: {
+      type: Sequelize.STRING,
+    },
+    first_name: {
+      type: Sequelize.STRING,
+    },
+    last_name: {
+      type: Sequelize.STRING,
+    },
+    father_name: {
+      type: Sequelize.STRING,
+    },
+    bio: {
+      type: Sequelize.TEXT,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, /* Sequelize*/) => queryInterface.dropTable('Employees'),
+};
